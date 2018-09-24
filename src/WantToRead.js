bdc21873b6efad203de878a80e1fc1519671571d
sophistique-chat <32949431+sophistique-chat@users.noreply.github.com>
2018-09-24 08:14:07 -0700

    fix:remove extra spaces

--- src/WantToRead.js
+++ src/WantToRead.js
@@ -15,11 +15,11 @@ class WantToRead extends Component {
                             this.props.books
                             .filter(book => book.shelf === 'wantToRead')
                             .map(book => (
-                                <li key = {book.id}>
+                                <li key={book.id}>
                                     <Book  
-                                    book = {book}
+                                    book={book}
                                     changeShelf={this.props.changeShelf}
-                                    currentShelf ="wantToRead"/>
+                                    currentShelf="wantToRead"/>
                                 </li>
                             ))
                         }
